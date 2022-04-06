9d86b4fb8f5e7a9554f197a04af6a51c16a31098
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-06 21:33:27 +0600

    login user info loaded

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
 
 const auth = getAuth(app)
 
 const App = () => {
-    // const provider = new GoogleAuthProvider()
+    const [user,setUser]=useState({})
 
-    // const handlegoogleSignIn = () =>{
-    //     signInWithPopup(auth, provider)
-    //     .then(result=>{
-    //         const user = result.user;
-    //         console.log(user)
-
-    //     })
-    //     .catch(error=>{
-    //         console.log(error)
-    //     })
-    // }
     const provider = new GoogleAuthProvider()
 
     const handlegoogleSignIn = () =>{
         signInWithPopup(auth, provider)
         .then(result=>{
             const user = result.user;
+            setUser(user)
             console.log(user);
         })
         .catch(error=>{
@@ -34,6 +24,7 @@ const App = () => {
     return (
         <div>
             <button onClick={handlegoogleSignIn}>google log in</button>
+            <h2>name:{user.displayName}</h2>
         </div>
     );
 };
