4dfe4622e11bce5c55647ef6b612281cbdd6bc49
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-07 16:07:53 +0600

    boostrap form for to take user info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 
 const auth = getAuth(app)
 
@@ -42,7 +46,17 @@ const App = () => {
              console.log(error)
          })
     }
-
+    {/* auth by input email password  */}
+const handleEmailBlur = (e) =>{
+    console.log(e.target.value)
+}
+const handlePasswordlBlur = (e) =>{
+    console.log(e.target.value)
+}
+const handleFormSubmit = (e) =>{
+    console.log('form submit')
+    e.preventDefault()
+}
     return (
         <div>
             {user.uid? <button onClick={handleSignOut}>google sign out</button>:
@@ -58,11 +72,30 @@ const App = () => {
             <p>email:{user.email}</p>
 
 
+{/* auth by input email password  */}
+    <div className="resistration w-50 mx-auto">
+        <h2 className="text-primary">plese resister</h2>
+    <Form onSubmit={handleFormSubmit}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
 
-            <form>
-                <input type="text" />
-                <input type="password" name="" id="" />
-            </form>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </div>
         </div>
     );
 };
